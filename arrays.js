e5e26f081be2b4b11d79e9d8fbbb1a4fbd024443
Muyale Okoyo <annmuyale@gmail.com>
2023-03-14 12:36:14 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,30 @@
+arr1 = [ 3 ,7 ,34 ,90 ,12];
+const lastItem = (arr1.slice(-1));
+console.log(lastItem);
+
+arr1 =[true, "green" ,"where" ,12 ,56];
+const reward = (arr1.slice(-1));
+console.log(reward);
+
+myPets =["cow", "Bird" ,"Snake" ,"Dog"];
+let text = myPets.join();
+console.log(text);
+
+
+ var arry= ["apple" ,"mango" ,"apple" ,"orange" ,"mango" ,"mango"];
+ 
+
+function remove(arr) {
+    return arry.filter(item,
+    index) =>arry.indexOf(item === index);
+
+}
+    console.log(remove(arry));
+
+var arr3 = [-5 , 9 ,5 , 2 ,-3 ,6 ,8,4 ,1];
+let sorted = arr3.sort((a,b) => a-b);
+    console.log(sorted);
+    
+
+
+
